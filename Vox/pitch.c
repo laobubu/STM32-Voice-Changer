03ec1d3ed4ad503d80af01eabfe84e2f7b797027
laobubu <laobubu@gmail.com>
2017-04-10 16:14:05 +0800

    改善低通滤波

--- Vox/pitch.c
+++ Vox/pitch.c
@@ -6,9 +6,12 @@
 static vox_fft_t fft = {{0}};
 vox_complex_t fft_tmp[VOX_FFTLEN];
 
-static float cutoff   = 10000;  	//Hz
+static float cutoff   = 20000;  	//Hz
 static float since    = 600;  	  //Hz
-static float shift    = -200;       //Hz
+static float shift    = 400;       //Hz
+
+static float lp_since = 10000;  // Hz, where decay starts
+static float lp_decay = 0.4;  // percent per bin
 
 int vox_init_pitch() {
 	vox_init_fft();
@@ -39,8 +42,25 @@ int vox_proc_pitch(vox_buf_t *buf){
 	vox_fft_begin(&fft, buf);
 	
 	int count = (cutoff - since) / VOX_FFTRES;
-	memcpy(fft_tmp, fft.fft + (int)(since/VOX_FFTRES), count);
-	memcpy(fft.fft + (int)((since+shift)/VOX_FFTRES), fft_tmp, count);
+	int readOffset = (since)/VOX_FFTRES;   // unit: bin
+	int writeOffset = (since+shift)/VOX_FFTRES;
+	
+	memcpy(fft_tmp, fft.fft + readOffset,  count);
+	memcpy(fft.fft + writeOffset, fft_tmp, count);
+	
+	//decay. LP
+	
+	writeOffset = lp_since/VOX_FFTRES;
+	vox_complex_t *c_i = fft.fft + writeOffset;
+	float p_i = 1.0f;
+	for (int i = writeOffset; i < VOX_FFTLEN; i++, c_i++) {
+		if (p_i < 0.01f) {
+			memset(c_i, 0, (VOX_FFTLEN - i)*sizeof(vox_complex_t));
+			break;
+		}
+		c_i->real *= p_i;
+		p_i *= lp_decay;
+	}
 	
 	vox_fft_end(&fft, buf);
 	return 0;
