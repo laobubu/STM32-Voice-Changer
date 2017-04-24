9b0b9d8bec8b0a99831a5ba62cb807b21fdf7c4a
laobubu <laobubu@gmail.com>
2017-04-24 21:09:10 +0800

    Adding LICENSE

--- Vox/vox-eq.c
+++ Vox/vox-eq.c
@@ -1,3 +1,24 @@
+/*!
+ * ==== STM32 VoiceChanger (vox) ====
+ * Simple equalizer for Complex-in-float based on line segments.
+ *
+ * written by laobubu, for SensorTile
+ * 
+ * Copyright 2017 laobubu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
 #include "vox-eq.h"
 
 void vox_eq_compile_line(vox_eq_line_t *line) {
--- Vox/vox-fft.c
+++ Vox/vox-fft.c
@@ -1,13 +1,22 @@
-/**
- * SensorTile Voice Process
- * Do FFT audio process in STM32.
- *  providing:
- *    - FFT/iFFT
- *    - Simple LPH/HPF
- *    - Interpolate on FFT data
+/*!
+ * ==== STM32 VoiceChanger (vox) ====
+ * FFT/iFFT class
  *
- *  (C) 2017 laobubu
+ * written by laobubu, for SensorTile
+ * 
+ * Copyright 2017 laobubu
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include "vox-fft.h"
